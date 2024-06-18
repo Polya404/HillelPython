a73122cef50a972a30228633f502a4ae005a781c
Polina <polyapolya404@gmail.com>
2024-06-18 22:30:24 +0300

    fix

--- lesson_04/task2.py
+++ lesson_04/task2.py
@@ -1,11 +1,12 @@
-list_of_numbers = [1, 16, 37, 17, 9, 0, 13, 2]
+list_of_numbers = [0, 1, 7, 2, 4, 8]
 
 ##### first solution #####
-print(sum(list_of_numbers[0::2]))
+print(sum(list_of_numbers[0::2]) * list_of_numbers[-1])
 
 ##### second solution #####
 sum_of_num = 0
 for num in range(0, len(list_of_numbers), 2):
     sum_of_num += list_of_numbers[num]
+sum_of_num *= list_of_numbers[-1]
 
 print(sum_of_num)
