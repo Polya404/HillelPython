62627ab2d72881bc350dcc9f3e17f93c0e9de6a9
Polina <polyapolya404@gmail.com>
2024-06-19 13:25:15 +0300

    added third solution for first task

--- lesson_04/task1.py
+++ lesson_04/task1.py
@@ -1,5 +1,8 @@
+import operator
+
 list_of_numbers = [1, 0, 5, 0, 0, -3, 0, 4]
 list_of_numbers_2 = [0, 11, 0, 5, 0, 0, -3, 73, -81, 0, 0, 4, 0]
+list_of_numbers_3 = [1, 0, -7, 0, 12, 0, 71, 0, -14, 0, -81, 0, 0, 4]
 
 ##### first solution #####
 zeros = list_of_numbers.count(0)
@@ -13,5 +16,10 @@ for i, num in enumerate(list_of_numbers_2):
         list_of_numbers_2.insert(len(list_of_numbers_2), num)
         list_of_numbers_2.remove(num)
 
+
+##### third solution #####
+list_of_numbers_3.sort(key=operator.not_)
+
 print(list_of_numbers)
 print(list_of_numbers_2)
+print(list_of_numbers_3)
