0a120d3814889e5185201c2ea926176b1f8edcf8
Polina <polyapolya404@gmail.com>
2024-07-02 11:55:43 +0300

    refactored

--- lesson_07/task4.py
+++ lesson_07/task4.py
@@ -1,6 +1,6 @@
 def common_elements():
-    multiples_of_3 = [x for x in range(100) if x % 3 == 0]
-    multiples_of_5 = [x for x in range(100) if x % 5 == 0]
+    multiples_of_3 = [x for x in range(0, 100, 3)]
+    multiples_of_5 = [x for x in range(0, 100, 5)]
     return set(multiples_of_3).intersection(set(multiples_of_5))
 
 
