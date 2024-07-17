8081fba257d088da99fb68b38b97e82f69506fd5
Polina <polyapolya404@gmail.com>
2024-07-17 17:24:52 +0300

    added 11 hw

---
+++ lesson_11/task2.py
@@ -0,0 +1,18 @@
+import math
+from inspect import isgenerator
+
+
+def generate_cube_numbers(end):
+    max_num = int(end ** (1 / 3)) + 1
+    for num in range(2, max_num + 1):
+        cube = num ** 3
+        if cube <= end:
+            yield cube
+
+
+gen = generate_cube_numbers(1)
+assert isgenerator(gen) == True, 'Test0'
+assert list(generate_cube_numbers(10)) == [8], 'оскільки воно менше 10.'
+assert list(generate_cube_numbers(100)) == [8, 27, 64], '5 у кубі це 125, а воно вже більше 100'
+assert list(generate_cube_numbers(1000)) == [8, 27, 64, 125, 216, 343, 512, 729, 1000], '10 у кубі це 1000'
+print('Ok')
